642f368786c6c64046a9733b63d05936e7ee95d8
admation <admation60@gmail.com>
2023-09-10 23:12:58 -0400

    finished MVP

--- lib/stores.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Writable } from 'svelte/store';
-import type { DOMWidgetModel } from '@jupyter-widgets/base';
-export declare function WidgetWritable<T>(name_: string, value_: T, model: DOMWidgetModel): Writable<T>;
-export declare const selectedStep: Writable<string>;
--- lib/utils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function deepCopy(obj: any): any;
